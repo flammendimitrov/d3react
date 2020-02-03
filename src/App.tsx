b6fb136bec852fd938614005eff874188ea6dde1
Plamen Dimitrov <plamen.dimitrov.github@gmail.com>
2020-02-03 13:43:33 +0100

    chart | created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from './Chart/Chart';
 
 const App = () => {
+  const [data, setData] = React.useState([79.6, 87.9, 94.6, 100.5, 105.5, 110.5, 115.7, 121, 126.6, 132.5, 138.4, 143.3, 146.7, 148.7, 149.8, 150.3, 150.6, 150.9]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <p>Letting React and D3.js play together</p>
+        <Chart data={data} />
+      </main>
     </div>
   );
 }
---
+++ src/Chart/Chart.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+type IPassedProps = {
+  data: number[]
+}
+
+export const Chart: React.FC<IPassedProps> = (props) => {
+  return (
+    <div>Chart</div>
+  );
+};
